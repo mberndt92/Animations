47602c631642bc1cc9be1316be06b153c25fe9dd
mberndt92 <maximilian_berndt@icloud.com>
2023-03-16 08:14:55 +0900

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Maximilian Berndt on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
